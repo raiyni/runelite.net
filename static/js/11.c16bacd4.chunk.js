ea0193ea0962feb3ce4c6997108bca1667e66a3a
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-10-11 12:07:30 +0000

    Deploy runelite/runelite.net to github.com/runelite/runelite.net.git:gh-pages

---
+++ static/js/11.c16bacd4.chunk.js
@@ -0,0 +1,2 @@
+webpackJsonp([11],{505:function(e,o){e.exports={title:"World Hopper plugin disabled",description:"World Hopper plugin disabled",author:"Adam",__content:"<p>Jagex has requested we remove the world hopper plugin because it is getting\nflagged by their bot detection. It has been removed until we can sort out how to\nfix it with them. I do not know why our world hopper is being flagged and the\nworld hopper on other clients is not.</p>\n<p>- Adam</p>\n"}}});
+//# sourceMappingURL=11.c16bacd4.chunk.js.map
